dd219f35cd9ed3b23af8ae1c0a7c9aebf16a5ba1
JoggerLad8872 <Jonny.Stewart24@bathspa.ac.uk>
2024-12-10 12:41:29 +0000

    Added basic example of class

--- Vending machine code/Vending machine/Vending machine/Main.cpp
+++ Vending machine code/Vending machine/Vending machine/Main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class FoodITM {
+public:
+	int ID;
+	string Name;
+	float price;
+	FoodITM(int a, string b, float c) {
+		ID = a;
+		Name = b;
+		price = c;
+	}
+};
+
 int main() {
+	FoodITM Drink1(1, "Coke", 3.99);
+
+	cout << Drink1.ID << " " << Drink1.Name << " " << Drink1.price << endl;
+
 
 	return 0;
 }
