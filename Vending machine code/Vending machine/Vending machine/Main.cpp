d5b1cb3b389667328483c2e2574c632948937945
JoggerLad8872 <Jonny.Stewart24@bathspa.ac.uk>
2024-12-10 13:11:04 +0000

    Made class/object interact with an array for easier use

--- Vending machine code/Vending machine/Vending machine/Main.cpp
+++ Vending machine code/Vending machine/Vending machine/Main.cpp
@@ -7,6 +7,10 @@ public:
 	int ID;
 	string Name;
 	float price;
+	//default constructor: A constructor is needed whenever an object is declared, if no constructor exists the program will not work properly
+	//the default constructor acts as an empty constcructor 
+	FoodITM() {};
+
 	FoodITM(int a, string b, float c) {
 		ID = a;
 		Name = b;
@@ -16,8 +20,16 @@ public:
 
 int main() {
 	FoodITM Drink1(1, "Coke", 3.99);
-
 	cout << Drink1.ID << " " << Drink1.Name << " " << Drink1.price << endl;
+
+
+
+	const int itemNum = 1;
+	FoodITM item[itemNum];
+	item[0] = FoodITM(1, "egg", 2.99);
+	cout << item[0].ID << endl;
+
+
 
 
 	return 0;
